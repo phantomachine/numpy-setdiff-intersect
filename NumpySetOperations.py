cfb5007563f12baba535c9cddb8110e4143c49e4
PhantoMachine <phantomachine@users.noreply.github.com>
2016-06-16 17:02:38 +1000

    Update NumpySetOperations.py

--- NumpySetOperations.py
+++ NumpySetOperations.py
@@ -1,29 +1,47 @@
 import numpy as np
 
-class tools:
+#---------- SET-DIFFERENCE OF ARRAYS -------------------------------------
+def setdiff(a1, a2):
+    """Simpler version of Matlab/R's SETDIFF for 2D arrays only """
+    a1 = a1.ravel().reshape(-1,a1.shape[-1])
+    a2 = a2.ravel().reshape(-1,a1.shape[-1])
+    a1_rows = a1.view([('', a1.dtype)] * a1.shape[-1])
+    a2_rows = a2.view([('', a2.dtype)] * a2.shape[-1])
+    ad = np.setdiff1d(a1_rows, a2_rows).view(a1.dtype).reshape(-1, a1.shape[-1])
+    index_bool = np.in1d(a1_rows, a2_rows, invert=True)
+    index = np.arange(index_bool.size)[index_bool]
+    return ad, index, index_bool
 
-    def setdiffn(self, array1, array2):
-        """Version of Matlab/R's 2D SETDIFF for ND arrays only. Reduces ND array down to 2D and returns 2D array (last two dims of ND arrays)
-        (c) GNU GPL2, 2016, T.Kam """
-        ncols = array1.shape[-1]
-        a = array1.ravel()
-        b = array2.ravel()
-        # Compare element-wise for similarity
-        c = np.in1d(a,b).reshape(array1.shape).sum(axis=-1,keepdims=True)
-        # index of array1 rows NOT contained in array 2
-        d = np.nonzero(c.ravel() != ncols)
-        # map index into 2D subset of 2D-reshaped version of array1:
-        return a.reshape(-1,ncols)[d], d
+#---------- INTERSECTION OF ARRAYS --------------------------------------
+def intersect(a1, a2):
+    """Simpler version of Matlab/R's INTERSECT for 2D arrays only """
+    a1 = a1.ravel().reshape(-1,a1.shape[-1])
+    a2 = a2.ravel().reshape(-1,a1.shape[-1])
+    a1_rows = a1.view([('', a1.dtype)] * a1.shape[-1])
+    a2_rows = a2.view([('', a2.dtype)] * a2.shape[-1])
+    ax=np.intersect1d(a1_rows, a2_rows).view(a1.dtype).reshape(-1, a1.shape[-1])
+    index_bool = np.in1d(a1_rows, a2_rows, invert=False)
+    index = np.arange(index_bool.size)[index_bool]
+    return ax, index, index_bool
 
-    def setintersectn(self,array1, array2):
-        """Simpler version of Matlab/R's INTERSECT for ND arrays. Reduces ND array down to 2D and returns 2D array (last two dims of ND arrays).
-        (c) GNU GPL2, 2016, T.Kam """
-        ncols = array1.shape[-1]
-        a = array1.ravel()
-        b = array2.ravel()
-        # Compare element-wise for similarity
-        c = np.in1d(a,b).reshape(array1.shape).sum(axis=-1,keepdims=True)
-        # index of array1 rows contained in array 2
-        d = np.nonzero(c.ravel() == ncols)
-        # map index into 2D subset of 2D-reshaped version of array1:
-        return a.reshape(-1,ncols)[d], d
+#------------- PYTHON LIST or NUMPY ARRAYS: Flatten vs. Reconstruct -------
+def flatten2(nl):
+    """
+    To flatten Python List of lists / numpy arrays (2 levels). (See also reverse operation in RECONSTRUCT() below.)
+    Usage: L_flat,l1,l2 = flatten2(L)
+    Source: http://stackoverflow.com/questions/27982432/flattening-and-unflattening-a-nested-list-of-numpy-arrays
+    """
+    l1 = [len(s) for s in itertools.chain.from_iterable(nl)]
+    l2 = [len(s) for s in nl]
+
+    nl = list(itertools.chain.from_iterable(itertools.chain.from_iterable(nl)))
+
+    return nl,l1,l2
+
+def reconstruct2(nl, l1, l2):
+    """
+    To reconstruct Python List of lists / numpy arrays. Inverse operation of FLATTEN() above.
+    Usage: L_reconstructed = reconstruct2(L_flat,l1,l2)
+    Source: http://stackoverflow.com/questions/27982432/flattening-and-unflattening-a-nested-list-of-numpy-arrays
+    """
+    return np.split(np.split(nl,np.cumsum(l1)),np.cumsum(l2))[:-1]
